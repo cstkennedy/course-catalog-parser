cfbba3c54447f467753975a6e511bbaaccf7e62e
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2019-05-26 14:37:21 -0400

    Add download/cache as gzipped html logic

--- get_listings.py
+++ get_listings.py
@@ -6,6 +6,7 @@ from collections import namedtuple
 import sys
 import re
 import os
+import gzip
 
 import odu_catalog.scraper as scraper
 
@@ -22,7 +23,7 @@ def _extract_prereq_courses(prereq_statement):
 def extract_usable_prereqs(courses):
 
     for course in courses:
-        print(course["prereqs"])
+        # print(course["prereqs"])
 
         if not course["prereqs"]:
             course["prereq_list"] = []
@@ -42,29 +43,51 @@ def find_is_required_for(course, all_courses):
     print()
 
 
-if __name__ == "__main__":
+def download_all_programs():
 
-    # base_page = download_page("http://catalog.odu.edu/courses/");
-    # extract_program_details(base_page)
-    base_catalog_url = "http://catalog.odu.edu/courses/cs/"
+    base_catalog_url = "http://catalog.odu.edu/courses/"
 
-    if os.path.exists("courses-cs.html"):
-        with open("courses-cs.html", "r") as subject_file:
-            the_page = subject_file.read()
+    if not os.path.exists("courses"):
+        os.mkdir("courses")
+
+    if os.path.exists("courses.html"):
+        with open("courses.html", "r") as courses_file:
+            base_page = courses_file.read()
 
     else:
+        base_page = scraper.download_page("http://catalog.odu.edu/courses/")
 
-        with open("courses-cs.html", "w") as subject_file:
-            the_page = scraper.download_page(base_catalog_url)
+    programs = scraper.extract_program_details(base_page)
 
-            subject_file.write(the_page)
+    for program in programs:
+        # Trim the trailing and leading slashes
+        file_path = program.path[1:-1] + ".html.gz"
+
+        print(program.abbrev.lower(), file_path)
+
+        program_url = base_catalog_url + program.abbrev.lower()
+
+        if not os.path.exists(file_path):
+            with gzip.open(file_path, "wt") as subject_file:
+                the_page = scraper.download_page(program_url)
+                subject_file.write(the_page)
+
+
+if __name__ == "__main__":
+
+    download_all_programs()
 
     pp = PrettyPrinter(indent=2, stream=sys.stderr)
+
+    # Analyze CS
+    with gzip.open("courses/cs.html.gz", "rt") as subject_file:
+        the_page = subject_file.read()
+
     courses = scraper.extract_course_details(the_page)
 
     extract_usable_prereqs(courses)
 
-    pp.pprint(courses)
+    # pp.pprint(courses)
 
     for course in courses:
         find_is_required_for(course, courses)
